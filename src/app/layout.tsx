44c96d8f3bafc3153bfa353a2370fe6d3915ba3e
ofikur <ofikur@users.noreply.github.com>
2025-07-29 14:53:50 +0700

    update title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "CV Generator",
+  title: "QuickCV - CV Generator",
   description: "Membuat CV profesional dengan mudah dan cepat",
 };
 
